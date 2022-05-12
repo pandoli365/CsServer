7a29afa7f72634cb244f3e5482638f4ded37f043
pandoli365 <jyp0036@gmail.com>
2022-05-12 14:02:37 +0900

    설명추가

--- SocketServerSystem/Program.cs
+++ SocketServerSystem/Program.cs
@@ -25,12 +25,33 @@ namespace SocketServerSystem
             }
         }
     }
+
+    /// <summary>
+    /// 샘플 코드 입니다 아래와같이 class를 새로 만들어 활용하셔도 좋고 udp를 직접 호출하셔서 사용하셔도 좋습니다
+    /// </summary>
     class SampleServerUDP : UDP
     {
+        //로비 List
         List<Lobby> LobbyList;
         //참조를 선언한 경우라면 가급적 udp의 직접 접근은 삼가해 주세요
         public static SampleServerUDP script;
-        public SampleServerUDP(int _Port = 4861) : base(true, _Port)
+
+        /// <summary>
+        /// base는 참조 선언한 클레스에 초기화 파라미터들을 전송해줄때 사용합니다.
+        /// _is_Server(true) : 현재 동작하는 시스템이 서버인지 클라이언트인지 구분하는 파라미터 (true : 서버 falst : 클라이언트)
+        /// _maxSize(1024) : 패킷의 최대 사이즈 1024가 기본값으로 설정되어 있습니다
+        /// _Port(_Port) : 접속을 허용할 혹은 접속할 네트워크 포트번호
+        /// _ip("127.0.0.1") : 접속하는 서버의 ip주소 (클라이언트로 선언했을때만 사용됩니다)
+        /// 생초보용 서버 세팅 방법
+        /// 공유기 사용시1 : 포트포워드 > 외부포트,내부포트를 이곳의 입력한 _Port와 같은번호로 설정한뒤 내부 ip주소를 실행하고 싶은 컴퓨터의 ip주소를 입력
+        /// 그뒤 클라이언트에 ip를 입력할때 서버컴퓨터와 연결되어있는 공유기 기본정보 외부ip입력 만약 찾기가 어렵다면 https://url.kr/web_tools/ip/ 접속
+        /// 모뎀 직접연결시 : https://url.kr/web_tools/ip/ 접속 나오는 ip를 클라이언트 접속ip에 입력
+        /// 
+        /// 만약 위의 방법으로 접속이 되지 않는다면 구글 검색창에 "방화벽 포트 열기"검색
+        /// 포트를 열어보고 다시시도
+        /// </summary>
+        /// <param name="_Port"></param>
+        public SampleServerUDP(int _Port = 4861) : base(true, 1024, _Port)
         {
             if (script == null)
             {
@@ -38,12 +59,18 @@ namespace SocketServerSystem
             }
             else
             {
+                //에러 글자색 변경을 위한 코드
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("에러 : 중복된 선언을 하셨습니다");
                 Console.ForegroundColor = ConsoleColor.White;
             }
         }
-        /*
+
+        /// <summary>
+        /// 데이터를 받았을 경우위 처리
+        /// 따로 호출하지 않아도 상대방이 데이터를 받으면 자동으로 동작합니다.
+        /// </summary>
+        /// <param name="di"></param>
         public override void v_Processing(DataInfo di)
         {
             UDPUser user;
@@ -124,9 +151,10 @@ namespace SocketServerSystem
                     break;
             }
         }
-        */
+
         public override void v_DataSet()
         {
+            //로비 리스트 초기화
             LobbyList = new List<Lobby>();
         }
         class Room
